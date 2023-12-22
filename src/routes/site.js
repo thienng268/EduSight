de7095a58835371a90aa4b5f8ef0f790a9eeef0f
theliemdayyy <liembuithe3@gmail.com>
2023-12-22 23:24:32 +0700

    add class module

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const siteControllers = require('../app/controllers/SiteController');
 //newControllers.index;
 router.use('/search', siteControllers.search);
+router.use('/theliem', siteControllers.theliem);
+
 router.use('/', siteControllers.index);
 
 

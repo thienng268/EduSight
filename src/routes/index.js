b45a1642eb1616bb1515fe620f9af75de8b0b1e5
Tlyl1111 <115882420+Tlyl1111@users.noreply.github.com>
2024-01-07 23:02:31 +0700

    add route account

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@
 const siteRouter = require('./site');
 const scheduleRouter = require('./schedule');
 const gradeRouter = require('./grade');
+const authRouter = require('./auth');
 
 function route(app)
 {
     app.use('/grade', gradeRouter);
     app.use('/schedule', scheduleRouter);
+    app.use('/auth', authRouter);
     app.use('/', siteRouter);
 }
 

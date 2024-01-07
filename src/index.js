2b0f078b194a0fcc27e42c7c732d72fabd1e125c
Tlyl1111 <115882420+Tlyl1111@users.noreply.github.com>
2024-01-07 18:19:12 +0700

    add package

--- src/index.js
+++ src/index.js
@@ -4,19 +4,48 @@ const handlebars = require('express-handlebars').engine;
 const path = require('path')
 const route = require('./routes');
 const db = require('./config/db');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+var favicon = require('serve-favicon')
 const app = express()
 const port = 3000
 
 //connect db
 db.connect();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false, // Đặt thành false cho môi trường không sử dụng HTTPS
+    maxAge: 1000 * 60 * 60 * 24 // Thời hạn cookie, ví dụ 24 giờ
+    // Các tùy chọn cookie khác nếu cần
+  },
+}));
 
+
+app.use((req, res, next) => {
+  if (req.session.user) {
+    res.locals.user = req.session.user;
+  };
+  next();
+});
+
+app.use(favicon(path.join(__dirname, 'public', 'img', 'LogoEduSight.png')))
+app.use('/src/public', express.static('src/public'));
 //HTTP logger
+
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Template engine
 app.engine('hbs', handlebars({extname: '.hbs',}),);
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 

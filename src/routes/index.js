8d23ed783ccfa18bc5386c53c97eaa840a561cce
nous366 <21521392@gm.uit.edu.vn>
2024-01-09 22:31:22 +0700

    func violationRecord

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,19 @@
-
+const violationRouter = require('./violation');
 const siteRouter = require('./site');
 const scheduleRouter = require('./schedule');
 const gradeRouter = require('./grade');
 const classRouter = require('./class');
 const authRouter = require('./auth');
 const teacherRouter = require('./teacher');
-const profileRouter = require('./profile');
 
 function route(app)
 {
+    app.use('/violation', violationRouter);
     app.use('/teacher', teacherRouter);
     app.use('/class', classRouter);
     app.use('/grade', gradeRouter);
     app.use('/schedule', scheduleRouter);
     app.use('/auth', authRouter);
-    app.use('/profile', profileRouter);
     app.use('/', siteRouter);
 }
 

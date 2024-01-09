feada33547389f3fe35f64c3b637386a05482dd1
theliemdayyy <liembuithe3@gmail.com>
2024-01-09 14:29:31 +0700

    fix violation

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -37,7 +37,7 @@ class SiteController
         res.render('search');
     }
     theliem(req, res) {
-        res.render('dashboard'); 
+        res.render('violation'); 
         //res.render('profile', { layout: 'non_sb' });
     }
 }

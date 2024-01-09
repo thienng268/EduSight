3d6c67f3520bf90e9a3843f85f58a6a015d02831
theliemdayyy <liembuithe3@gmail.com>
2024-01-09 15:10:47 +0700

    add homeroom

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -37,7 +37,7 @@ class SiteController
         res.render('search');
     }
     theliem(req, res) {
-        res.render('violation'); 
+        res.render('homeroom'); 
         //res.render('profile', { layout: 'non_sb' });
     }
 }

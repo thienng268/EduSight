12cf68eef348a670d64516d80d7da7ad147b5645
Nguyen Thien <thiennt268@gmail.com>
2024-01-10 09:39:21 +0700

    dev: fix add student

--- src/app/controllers/ClassController.js
+++ src/app/controllers/ClassController.js
@@ -78,6 +78,28 @@ class ClassController
             res.status(500).send('Error creating class and students');
           }
     }
+    async create(req, res) {
+        try {
+            const classId = req.query.classId;
+            console.log(classId);
+            //console.log(classId);
+            const newStudent = new Student({
+                Name: req.body.StudentName,
+                Address: req.body.Address,
+                ParentName: req.body.ParentName,
+                ParentPhone: req.body.PhoneNumber,
+                DOB: req.body.Date,
+                Status: req.body.Subject,
+                classID: classId,
+            });
+          await newStudent.save();
+          
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Eror: '+ error);
+        }
+        
+    }
 }
 
 module.exports = new ClassController;

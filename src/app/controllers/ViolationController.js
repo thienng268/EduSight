17db3d8ad9123cdaf552697f4e8dac226a0ae9f3
Nguyen Thien <thiennt268@gmail.com>
2024-01-12 01:29:36 +0700

    dev: Sort top 5 in class

--- src/app/controllers/ViolationController.js
+++ src/app/controllers/ViolationController.js
@@ -2,6 +2,40 @@ const Teacher = require('../models/Teacher');
 const Class = require('../models/Class');
 const Subject = require('../models/Subject');
 const Violation = require('../models/Violation');
+const Student = require('../models/Student');
+const Semester = require('../models/Semester');
+
+
+async function getStudentInfo(classId, Num) {
+    try {
+        const students = await Student.find({ classID: classId }).select('_id Name DOB').lean();
+        const studentsWithDTB = await Promise.all(students.map(async (student) => {
+            const studentId = student._id;
+            const studentName = student.Name;
+            const studentDOB = student.DOB;
+            const dtb = await getDTBFromSemester(studentId, Num);
+            return {
+                Name: studentName,
+                DOB: studentDOB,
+                DTB: dtb
+            };
+        }));
+        studentsWithDTB.sort((a, b) => (b.DTB || 0) - (a.DTB || 0));
+        const top5Students = studentsWithDTB.slice(0, 5);
+        return top5Students;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+async function getDTBFromSemester(studentId, semesterNum) {
+    const semester = await Semester.findOne({
+        studentID: studentId,
+        semesterNum: semesterNum
+    }).select('DTB').lean();
+
+    return semester ? semester.DTB : 0;
+}
 
 class ViolationController
 {
@@ -10,10 +44,14 @@ class ViolationController
             const violations = await Violation.find({})
                 .select('NameStudent Class Violation Date').lean();
             const classes = await Class.find({})
-                .select('Name').lean();
-            res.render('violation', { violations, classes });
+                .select('Name _id').lean();
+            const classId = req.query.classID;
+            const semesterNum = req.query.semesterNum;
+            const Num = semesterNum === '2' ? 2 : 1;
+            const studentData = await getStudentInfo(classId, Num);
+            res.render('violation', { students: studentData , violations, classes });
         } catch (error) {
-            console.error(error);
+            console.error("Lỗi trong phương thức index:", error);
             res.status(500).send('Có lỗi xảy ra');
         }
     }
